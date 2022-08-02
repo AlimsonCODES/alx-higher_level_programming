9e8c4fc291284d7420a47760a6e256540978c766
toluwani <alimsondaniel@gmail.com>
2022-08-01 18:28:32 -0700

    cool

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,5 +4,7 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as err:
-        print("Exception:", err)
+        sys.stderr.write("Exception: ")
+        sys.stderr.write(err.args[0])
+        sys.stderr.write("\n")
         return False
