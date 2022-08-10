5daffaf99ff280d710005a5dec62eef0826f8005
Daniel <alimsondaniel@gmail.com>
2022-08-10 00:59:39 -0700

    lols

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+"""
+this would represent a module for
+working with squares
+"""
+
+
+class Square:
+
+    """
+    a plane figure with four equal straight sides
+    and four right angles.
+    """
+    def __init__(self, size=0):
+        """
+        Initialize a new Square.
+            Args:
+                size (int): The size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """get/set the object size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+
+    def area(self):
+        """
+        calculates area
+        """
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """
+        print '#' for area of square
+        """
+        if size == 0:
+            print("")
+        else:
+            for i in range(size):
+                for num in range(size):
+                    print("#", end='')
+                print("")
