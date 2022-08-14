0ac70d8dd36b6e944a8a967e8243a2e811c8748b
Daniel <alimsondaniel@gmail.com>
2022-08-13 21:46:11 -0700

    okay6

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+"""Module for a rectangle.
+"""
+
+
+class Rectangle:
+    """Class for rectangle 2d object with 4 sides.
+    """
+    def __init__(self, width=0, height=0):
+        """Initialises the rectangle
+
+        Args:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get/set for width
+
+        Returns:
+            int: width for triangle.
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """Get/set for height.
+
+        Returns:
+            int: height for triangle.
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """Setter for width
+
+        Args:
+            value (int): new width of the triangle.
+        """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Setter for height
+        Args:
+           value (int): new height of the triangle.
+        """
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        """Defines the area of a rectangle.
+        Returns:
+            int: the new area of triangle.
+        """
+        return self.height * self.width
+
+    def perimeter(self):
+        """Function that returns the perimeter of a triangle.
+        Returns:
+            int: perimeter of triangle.
+        """
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return 2 * (self.width + self.height)
+
+    def __str__(self)
+        "function that prints str.
+        Retuns:
+            str: unoficial return.
+        """
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            res = list(map(
+                lambda x: '#' * self.width + '\n' * (x != self.height - 1),
+                range(self.height)))
+            return ''.join(res)
