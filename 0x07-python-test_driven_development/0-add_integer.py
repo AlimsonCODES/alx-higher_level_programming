38d3281ee89025578fdc03fb9724152ad09d0c8c
Daniel <alimsondaniel@gmail.com>
2022-08-18 18:13:02 -0700

    okay1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
-
-"""A module for addition of 2 integers.
-"""
-
-
 def add_integer(a, b=98):
-    """A function that adds 2 integers.
-        Args:
-            a (int, float): arg 1.
-            b (int, float): arg 2.
+    """A function that adds 2 integer"""
 
-        Returns:
-            int: sum of two integers.
-    """
     if not a or (type(a) != int and type(a) != float):
         raise TypeError('a must be an integer')
     if type(b) != int and type(b) != float:
