067d7b2be5048161c1a324a5553d17d6ba8797fe
Daniel <alimsondaniel@gmail.com>
2022-08-30 23:19:15 -0700

    this is another one

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,8 +4,8 @@
 
 class Mylist(list):
     """ class module that recieves a list"""
-
     def print_sorted(self):
+        """ must document func"""
         sorted_list = sorted(self)
         print(sorted_list)
         del sorted_list
