24f4060cb1d2b45bd4ac590e2218e443c0f6dc93
Daniel <alimsondaniel@gmail.com>
2022-08-30 21:58:47 -0700

    yayy

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" module """
+
+
+class BaseGeometry:
+    """ yeah yeah"""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" module """
+
+
+class BaseGeometry:
+    """ yeah yeah"""
+    def area(self):
+        raise Exception("area() is not implemented")
